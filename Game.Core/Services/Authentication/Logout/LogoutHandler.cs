e024d8c99f81f0fc35fd6366f3dac792a5d2bb54
zenraikou <zenraikou@gmail.com>
2023-06-05 19:02:30 +0800

    modify logout service

--- Game.Core/Services/Authentication/Logout/LogoutHandler.cs
+++ Game.Core/Services/Authentication/Logout/LogoutHandler.cs
@@ -1,7 +1,9 @@
 using Game.Contracts.Session;
+using Game.Core.Common.Headers;
 using Game.Core.Common.JWT;
 using Game.Core.Exceptions;
 using Game.Core.Services.Claims;
+using Game.Core.Services.Header;
 using Game.Core.Services.Sessions.Delete;
 using Game.Core.Services.Sessions.Get;
 using MapsterMapper;
@@ -20,15 +22,18 @@ public class LogoutHandler : IRequestHandler<LogoutCommand, Unit>
         _mapper = mapper;
     }
 
-    public async Task<Unit> Handle(LogoutCommand request, CancellationToken cancellationToken)
+    public async Task<Unit> Handle(LogoutCommand request, CancellationToken cancellatioSnToken)
     {
         var getClaimQuery = new GetClaimQuery(c => c.Type == JWTClaims.JTI);
         var jti = await _mediator.Send(getClaimQuery);
 
+        var getHeaderQuery = new GetHeaderQuery(Headers.Fingerprint);
+        var fingerprint = await _mediator.Send(getHeaderQuery);
+
         var getSessionQuery = new GetSessionQuery(s => s.JTI == jti);
         var sessionResponse = await _mediator.Send(getSessionQuery);
 
-        if (sessionResponse is null)
+        if (sessionResponse is null || sessionResponse.Fingerprint != fingerprint)
         {
             throw new UnauthorizedException("Access denied.");
         }
